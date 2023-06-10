ce1464662d25484acd522bdd44dedfd8c3a725b2
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-06-10 17:40:03 +0100

    modifyTableEntry function done

--- public/js/modifyTableEntry.js
+++ public/js/modifyTableEntry.js
@@ -1,5 +1,15 @@
 import seeTable from './seeTable.js';
 
 export default function modifyTableEntry(task) {
-  
+  const rowIndex = task.parentNode.parentNode.rowIndex - 1;
+  const taskList = JSON.parse(localStorage.getItem('taskList')) || [];
+
+  const newTask = prompt("introduzca la tarea por la que remplazaremos la anterior");
+  if (newTask.trim() !== "") {
+    taskList[rowIndex] = newTask;
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+    seeTable();
+  } else {
+    alert("No se puede agregar una entrada vacía");
+  }
 }
